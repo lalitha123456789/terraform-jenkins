00b3dc592bea2c2b500ba35798cd9c762c8c9946
lalitha123456789 <168406131+lalitha123456789@users.noreply.github.com>
2024-05-24 17:14:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name" {
-    ami = ami-0cc9838aa7ab1dce7
-    instance_type = t2.micro
-    key_name = keypair1
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    key_name = "keypair1"
 
     tags = {
       Name = "newtag"
